9582e3a657799a1f90a996815ab7207b87c6f520
Zico郭 <21520993@qq.com>
2017-11-24 18:08:14 +0800

    0.0.1.3

--- src/App.config.js
+++ src/App.config.js
@@ -2,88 +2,88 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2017-11-19 00:24:55 
  * @Last Modified by: Azusakuo
- * @Last Modified time: 2017-11-21 10:58:33
+ * @Last Modified time: 2017-11-24 11:06:13
  */
 
 //  开发模式
 let is_dev = 0;
-let devReg = new RegExp('(127\.0\.0\.1.*?)|(localhost.*?)', 'i');
+let devReg = new RegExp("(127.0.0.1.*?)|(localhost.*?)", "i");
 
 if (devReg.test(window.location.host)) {
-	console.log('本地开发');
-	is_dev = 1
+  console.log("本地开发");
+  is_dev = 1;
 }
 //  根目录
-let rootPath = require('path').resolve(__dirname, 'src');
+let rootPath = require("path").resolve(__dirname, "src");
 //  服务器地址(api接口)
-let serverUrl = is_dev ? '/server/' : 'http://design.logods.cn/public/'; //http://design.logods.cn
+let serverUrl = is_dev ? "/server/" : "https://design.logods.cn/public/"; //http://design.logods.cn
 
 let configObject = {
-	//  开发模式
-	dev: is_dev,
+  //  开发模式
+  dev: is_dev,
 
-	//	系统默认加载admin和website2个模块
+  //	系统默认加载admin和website2个模块
 
-	//	后台模块
-	adminApp: 'admin',
-	//  后台模块中需要使用到的app模块名(文件夹名)
-	adminApps: ['design'],
-	//	前台模块
-	websiteApp: 'website',
-	//	前台模块中需要使用到app模块(文件夹名)
-	websiteApps: [],
+  //	后台模块
+  adminApp: "admin",
+  //  后台模块中需要使用到的app模块名(文件夹名)
+  adminApps: ["design"],
+  //	前台模块
+  websiteApp: "website",
+  //	前台模块中需要使用到app模块(文件夹名)
+  websiteApps: [],
 
-	//  项目根目录
-	path: {
-		rootPath: rootPath,
-		publicPath: rootPath + 'public-resource/',
-		appPath: rootPath + 'apps/'
-	},
-	//  项目地址
-	url: {
-		rootUrl: '/',
-		serverUrl: serverUrl,
-		adminUrl: serverUrl + 'admin/'
-	},
+  //  项目根目录
+  path: {
+    rootPath: rootPath,
+    publicPath: rootPath + "public-resource/",
+    appPath: rootPath + "apps/"
+  },
+  //  项目地址
+  url: {
+    rootUrl: "/",
+    serverUrl: serverUrl,
+    adminUrl: serverUrl + "admin/"
+  },
 
-	//  动态添加路径
-	pushPath: (name, path) => {
-		configObject.path[name] = path;
-	},
+  //  动态添加路径
+  pushPath: (name, path) => {
+    configObject.path[name] = path;
+  },
 
-	//  动态添加地址
-	pushUrl: (name, path) => {
-		configObject.url[name] = path;
-	}
+  //  动态添加地址
+  pushUrl: (name, path) => {
+    configObject.url[name] = path;
+  }
 };
 let config = {
-	is_dev: is_dev
+  is_dev: is_dev
 };
-config.install = function (Vue) {
-	/**
-	 * 获取配置中的路径
-	 * @param name
-	 * @returns {*|null}
-	 */
-	Vue.prototype.$getPath = name => {
-		return configObject.path[name] || null;
-	};
-	/**
-	 * 获取配置中的地址
-	 * @param name
-	 * @returns {*|null}
-	 */
-	Vue.prototype.$getUrl = name => {
-		return configObject.url[name] || null;
-	};
-	/**
-	 * 开发模式
-	 * @deprecated 被is_dev取代
-	 * @returns {number}
-	 */
-	Vue.prototype.$isDev = () => {
-		return is_dev;
-	};
+config.install = function(Vue) {
+  /**
+   * 获取配置中的路径
+   * @param name
+   * @returns {*|null}
+   */
+  Vue.prototype.$getPath = name => {
+    return configObject.path[name] || null;
+  };
+  /**
+   * 获取配置中的地址
+   * @param name
+   * @returns {*|null}
+   */
+  Vue.prototype.$getUrl = name => {
+    return configObject.url[name] || null;
+  };
+  /**
+   * 开发模式
+   * @deprecated 被is_dev取代
+   * @returns {number}
+   */
+  Vue.prototype.$isDev = () => {
+    return is_dev;
+  };
 };
 
 export default configObject;
--- webpack.config.js
+++ webpack.config.js
@@ -10,115 +10,139 @@ const publicPath = "/assets/";
 const itemName = "dist";
 
 module.exports = (options = {}) => {
-	return {
-		entry: {
-			vendor: "./src/vendor",
-			index: "./src/main.js",
-			common: ["vue", "vue-router", "vue-resource"],
-			elementui: ["element-ui"]
-		},
-		output: {
-			path: resolve(__dirname, itemName),
-			filename: "js/[name].js?[hash]",
-			chunkFilename: "js/chunk[id].js?[hash]",
-			publicPath: publicPath
-		},
-		module: {
-			rules: [{
-				test: /\.vue$/,
-				use: ["vue-loader"]
-			}, {
-				test: /\.js$/,
-				use: [{
-					loader: "babel-loader",
-					options: {
-						presets: [
-							["es2015", {
-								modules: false
-							}]
-						],
-						plugins: ["syntax-dynamic-import"]
-					}
-				}],
-				exclude: /node_modules/
-			}, {
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: ["style-loader", "postcss-loader"],
-					use: "css-loader"
-				})
-			}, {
-				test: /\.(scss|sass)$/,
-				use: ExtractTextPlugin.extract("sass-loader"),
-				exclude: /node_modules/
-			}, {
-				test: /\.(png|jpg|jpeg|gif|svg|svgz)$/,
-				use: [{
-					loader: "url-loader",
-					options: {
-						limit: 8129
-					}
-				}, {
-					loader: "file-loader",
-					options: {
-						name: "[name]_[hash:6].[ext]",
-						outputPath: "images/"
-					}
-				}],
-				exclude: /node_modules/
-			}, {
-				test: /\.(eot|ttf|woff|woff2)$/,
-				use: [{
-					loader: "file-loader",
-					options: {
-						name: "[name]_[hash:8].[ext]",
-						outputPath: "fonts/"
-					}
-				}]
-			}]
-		},
-		plugins: [new webpack.optimize.CommonsChunkPlugin({
-			names: ["vendor", "manifest", "common", "elementui"]
-		}), new HtmlWebpackPlugin({
-			template: "src/index.html"
-		}), new ExtractTextPlugin({
-			filename: "[name].[hash:5].css",
-			allChunks: true
-		}), new webpack.optimize.UglifyJsPlugin({
-			mangle: {
-				except: ["$super", "$", "exports", "require"]
-			}
-		}), new webpack.optimize.DedupePlugin()],
-		resolve: {
-			alias: {
-				"~": resolve("src"),
-				"~app": resolve("src/apps"),
-				vue: "vue/dist/vue.js"
-			}
-		},
-		devServer: {
-			host: "127.0.0.1",
-			port: 8010,
-			proxy: {
-				"/api/": {
-					target: "http://127.0.0.1:8080",
-					changeOrigin: true,
-					pathRewrite: {
-						"^/api": ""
-					}
-				},
-				"/server/": {
-					target: "http://tiramisu.localhost.com",
-					changeOrigin: true,
-					pathRewrite: {
-						"^/server": ""
-					}
-				}
-			},
-			historyApiFallback: {
-				index: url.parse(publicPath).pathname
-			}
-		},
-		devtool: options.dev ? "#eval-source-map" : "#source-map"
-	}; // 'vue': 'vue/dist/vue.min.js', //  别名vue,用作动态解释模板
+  return {
+    entry: {
+      vendor: "./src/vendor",
+      index: "./src/main.js",
+      common: ["vue", "vue-router", "vue-resource"],
+      elementui: ["element-ui"]
+    },
+    output: {
+      path: resolve(__dirname, itemName),
+      filename: "js/[name].js?[hash]",
+      chunkFilename: "js/chunk[id].js?[hash]",
+      publicPath: publicPath
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: ["vue-loader"]
+        },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  [
+                    "es2015",
+                    {
+                      modules: false
+                    }
+                  ]
+                ],
+                plugins: ["syntax-dynamic-import"]
+              }
+            }
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: ["style-loader", "postcss-loader"],
+            use: "css-loader"
+          })
+        },
+        {
+          test: /\.(scss|sass)$/,
+          use: ExtractTextPlugin.extract("sass-loader"),
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|svgz)$/,
+          use: [
+            // 	{
+            // 	loader: "url-loader",
+            // 	options: {
+            // 		limit: 8129,
+            // 		fallback: "responsive-loader"
+            // 	}
+            // },
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name]_[hash:6].[ext]",
+                outputPath: "images/"
+              }
+            }
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name]_[hash:8].[ext]",
+                outputPath: "fonts/"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ["vendor", "manifest", "common", "elementui"]
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/index.html"
+      }),
+      new ExtractTextPlugin({
+        filename: "[name].[hash:5].css",
+        allChunks: true
+      }),
+      // new webpack.optimize.UglifyJsPlugin({
+      //   exclude: /node_modules/,
+      //   mangle: {
+      //     except: ["$super", "$", "exports", "require"]
+      //   }
+      // }),
+    ],
+    resolve: {
+      alias: {
+        "~": resolve("src"),
+        "~app": resolve("src/apps"),
+        vue: "vue/dist/vue.js"
+      }
+    },
+    devServer: {
+      host: "127.0.0.1",
+      port: 8010,
+      proxy: {
+        "/api/": {
+          target: "http://127.0.0.1:8080",
+          changeOrigin: true,
+          pathRewrite: {
+            "^/api": ""
+          }
+        },
+        "/server/": {
+          target: "http://tiramisu.localhost.com",
+          changeOrigin: true,
+          pathRewrite: {
+            "^/server": ""
+          }
+        }
+      },
+      historyApiFallback: {
+        index: url.parse(publicPath).pathname
+      }
+    },
+    devtool: options.dev ? "#eval-source-map" : "#source-map"
+  }; // 'vue': 'vue/dist/vue.min.js', //  别名vue,用作动态解释模板
 };
